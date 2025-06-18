d856cce072462441cd532bffbbf791b029f5ed16
shelesh <sheleshsingh755@gmail.com>
2025-06-18 13:23:05 +0530

    update database connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,6 @@ import { config as conf } from 'dotenv';
 conf();
 const _config = {
   databaseUrl: process.env.MONGODB_URI,
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8000,
 };
 export const config = Object.freeze(_config);
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,24 @@
 import mongoose from 'mongoose';
 import { config } from './config';
+
+mongoose.set('strictQuery', true);
+let isListenerAttach = false;
+
 const connectDb = async () => {
   try {
-    mongoose.connection.on('connected', () => {
-      console.log('Database connected successfully');
-    });
-    mongoose.connection.on('error', (err) => {
-      console.error('Database connection error:', err);
-    });
+    if (!isListenerAttach) {
+      mongoose.connection.on('connected', () => {
+        console.log('Database connected successfully');
+      });
+      mongoose.connection.on('error', (err) => {
+        console.error('Database connection error:', err);
+      });
+      mongoose.connection.on('disconnected', () => {
+        console.log('Database disconnected');
+      });
+      isListenerAttach = true;
+    }
+
     await mongoose.connect(config.databaseUrl as string);
   } catch (error) {
     console.error('Database connection error:', error);
