93e7df5fddd1edbf80a010e9fccfcf360c4a06e1
shelesh <sheleshsingh755@gmail.com>
2025-06-18 11:25:17 +0530

    start backend project

--- index.ts
+++ index.ts
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World! test');
+  res.send('Hello World from Doctor App!');
 });
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
